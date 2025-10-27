3d90a4c380bd963196d2f585886c7aed652700ea
ashutoshgupt243 <vnsashutoshit.gmail.com>
2025-10-27 10:06:27 +0530

    Xcounter

---
+++ Xcounter/src/App.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react"
+
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment() {
+
+    this.setState({ count: this.state.count + 1 })
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+  
+
+
+  render() {
+    return (<>
+      <h1>Counter App</h1>
+      <div>
+        count:{this.state.count}
+      </div>
+      <div>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+      </div>
+    </>);
+  }
+
+}
+export default App;
